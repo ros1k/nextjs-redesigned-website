dacac923d33b6a2bcf73fe0d6015218b4e87060d
Damian <rosa.damian@gmail.com>
2021-02-19 16:30:20 +0100

    feat: emailjs for contact form added

--- components/Contact/ContactForm/ContactForm.jsx
+++ components/Contact/ContactForm/ContactForm.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import style from './ContactForm.module.scss'
 import {motion} from 'framer-motion'
+import emailjs from 'emailjs-com';
+
 const ContactForm = () => {
+   const [messageState,setMessageState] = useState(null);
+   const [animationState, setAnimationState] = useState({opacity:0,y:10})
+   useEffect(()=>{
+      if(messageState === true  || messageState  === false) {
+         setAnimationState({
+            opacity:1,
+            y:0,
+            tranisiton:{
+               duration:0.5,
+               delay:0.2,
+            }
+         })
+      }
+     
+   },[messageState])
+   const sendEmail = (e) =>{
+      e.preventDefault();
+      // emailjs.sendForm(process.env.EMAILJS_SERVICE, process.env.EMAILJS_TEMPLATE, e.target, process.env.EMAILJS_USER)
+       emailjs.sendForm(process.env.EMAILJS_SERVICE, process.env.EMAILJS_TEMPLATE, e.target, process.env.EMAILJS_USER)
+        .then((result) => {
+            console.log(result.text);
+            result.text === 'OK' ? setMessageState(true) : setMessageState(false);
+            console.log(messageState);
+        }, (error) => {
+            setMessageState(false);
+            console.log(error.text);
+        });
+   }
    return (
-      <form className={style['contact-form']} name="contact" method="POST">
+      <form className={style['contact-form']} name="contact" onSubmit={sendEmail}>
          <div className={style['contact-form-details']}>
             <input type="hidden" name="form-name" value="contact" />
             <p>
@@ -35,8 +65,23 @@ const ContactForm = () => {
                      y:-3,
                      x:2
                   }}
-                  type="submit">Send</motion.button>
+
+                  type="submit" >Send</motion.button>
             </p>
+            <motion.p className={
+               messageState === true?style['contact-form-details__message-state--send']:
+               messageState === false?style['contact-form-details__message-state--error']:
+               style['contact-form-details__message-state']} 
+            initial={{
+              
+               opacity:0,
+               y:10,
+            }}
+            animate={animationState}>
+               {messageState === null ? "" : 
+               messageState === true? 'Your message was sent succesfuly':
+               messageState === false? 'Ops something went wrong try again later':null}
+            </motion.p>
         </div>
         
     </form>    
